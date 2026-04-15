ca18b1337461c14ff21467a2a79036891570d225
uuaxe <cweixin@gmail.com>
2026-04-15 15:16:03 +0800

    batch correct LYRIKS dataset (excl. outliers)

--- notebooks/01-preprocess.R
+++ notebooks/01-preprocess.R
@@ -28,66 +28,60 @@ metadata$collection_datetime <- as.POSIXct(
 
 ### Preprocess data ###
 
-# Subset data
+## Subset data
+
+# # Identifying outlier according to runtime
+# subset(
+#   metadata,
+#   extraction_date == '28/8/24' & group == 'Healthy control',
+#   select = 'run_datetime'
+# )
 
 ### Psychosis signature (cvt, mnt, ctrl) ###
 
-outliers_sn <- c('L0626C', 'L0018C')
-cmc <- subset_cols(
+outliers_sn <- c('L0076C', 'L0018C', 'L0626C')
+lyriks393 <- subset_cols(
   lyriks_full,
   metadata,
-  group %in% c("Convert", "Maintain", "Healthy control") &
   !(sn %in% outliers_sn)
 )
+print(dim(lyriks393))
 
-metadata_cmc <- metadata[colnames(cmc), ]
+metadata393 <- metadata[colnames(lyriks393), ]
 # Relevel with convert batch as reference
-metadata_cmc$extraction_date <- droplevels(metadata_cmc$extraction_date)
-print(levels(metadata_cmc$extraction_date))
+metadata393$extraction_date <- droplevels(metadata393$extraction_date)
+print(levels(metadata393$extraction_date))
 
 # Convert to days since earliest 
-metadata_cmc$run_datetime_days <- as.numeric(difftime(
-  metadata_cmc$run_datetime,
-  min(metadata_cmc$run_datetime, na.rm = TRUE),
+metadata393$run_datetime_days <- as.numeric(difftime(
+  metadata393$run_datetime,
+  min(metadata393$run_datetime, na.rm = TRUE),
   units = "days"
 ))
-metadata_cmc$collection_datetime_days <- as.numeric(difftime(
-  metadata_cmc$collection_datetime,
-  min(metadata_cmc$collection_datetime, na.rm = TRUE),
+metadata393$collection_datetime_days <- as.numeric(difftime(
+  metadata393$collection_datetime,
+  min(metadata393$collection_datetime, na.rm = TRUE),
   units = "days"
 ))
-# metadata_cmc$collection_datetime_centered <- 
-#   metadata_cmc$collection_datetime_days - mean(metadata_cmc$collection_datetime_days)
-# mod <- model.matrix(~group, data = metadata_cmc)
+# metadata393$collection_datetime_centered <- 
+#   metadata393$collection_datetime_days - mean(metadata393$collection_datetime_days)
+# mod <- model.matrix(~group, data = metadata393)
 
 ### Subset control samples for slope estimation ###
 
 ctrl_0409 <- subset_cols(
-  lyriks_full,
+  lyriks393,
   metadata,
   group == 'Healthy control'
     & extraction_date == '4/9/24'
     & !(sn %in% outliers_sn)
 )
-metadata_ctrl_0409 <- metadata_cmc[colnames(ctrl_0409), ] 
-
-# # Convert to days since earliest 
-# metadata_ctrl_0409$collection_datetime_days <- as.numeric(difftime(
-#   metadata_ctrl_0409$collection_datetime,
-#   min(metadata_ctrl_0409$collection_datetime, na.rm = TRUE),
-#   units = "days"
-# ))
-# metadata_ctrl_0409$collection_datetime_days <- as.numeric(difftime(
-#   metadata_ctrl_0409$collection_datetime,
-#   min(metadata_ctrl_0409$collection_datetime, na.rm = TRUE),
-#   units = "days"
-# ))
+metadata_ctrl_0409 <- metadata393[colnames(ctrl_0409), ] 
 
 mod <- model.matrix(
    ~ run_datetime_days + collection_datetime_days,
    data = metadata_ctrl_0409
 )
-
 fit <- lmFit(ctrl_0409, mod)
 betas <- coef(fit)
 
@@ -101,60 +95,43 @@ betas <- coef(fit)
 
 ### Batch correction for continuous covariates on entire CMC subset ###
 
-cmc_corr <- cmc - outer(
+lyriks393_corr <- lyriks393 - outer(
   betas[, "run_datetime_days"],
-  metadata_cmc$run_datetime_days
+  metadata393$run_datetime_days
 ) - outer(
   betas[, "collection_datetime_days"],
-  metadata_cmc$collection_datetime_days
+  metadata393$collection_datetime_days
 )
 
-file <- 'data/tmp/cmc-lm.csv'
-write.csv(cmc_corr, file)
-print(file)
 
 ### Batch correct for extraction date ###
 
 # ComBat correction
-print(table(metadata_cmc$extraction_date, metadata_cmc$group))
-mod_group <- model.matrix(~ group, data = metadata_cmc)
+print(table(metadata393$extraction_date, metadata393$group))
+mod_group <- model.matrix(~ group, data = metadata393)
 
-mc_combat <- ComBat(
-  cmc_corr,
-  batch = metadata_cmc$extraction_date,
+lyriks393_combat <- ComBat(
+  lyriks393_corr,
+  batch = metadata393$extraction_date,
   mod = mod_group,
   par.prior = TRUE,
   ref.batch = '4/9/24'
 )
 
-file <- 'data/tmp/cmc-combat_0409.csv'
-write.csv(cmc_combat, file)
+file <- 'data/tmp/lyriks393-combat_0409.csv'
+write.csv(lyriks393_combat, file)
 print(file)
 
-cmc_combat <- ComBat(
-  cmc_corr,
-  batch = metadata_cmc$extraction_date,
-  mod = mod_group,
-  par.prior = TRUE,
-  ref.batch = '5/9/24'
-)
 
-file <- 'data/tmp/cmc-combat_0509.csv'
-write.csv(cmc_combat, file)
-print(file)
-
-# Limma correction (linear model, no Bayesian shrinkage)
-# Assumption: No interaction between batch effects due to run_datetime and extraction_date
-metadata_cmc$extraction_date <- relevel(
-  metadata_cmc$extraction_date,
-  ref = '4/9/24'
-)
-cmc_limma <- removeBatchEffect(
-  cmc_corr,
-  batch = metadata_cmc$extraction_date,
-  design = mod_group 
-)
-
-file <- 'data/tmp/cmc-limma.csv'
-write.csv(cmc_limma, file)
-print(file)
+# ## Alternative: Limma correction (linear model, no Bayesian shrinkage)
+# # Assumption: No interaction between batch effects due to run_datetime
+# # and extraction_date
+# metadata393$extraction_date <- relevel(
+#   metadata393$extraction_date,
+#   ref = '4/9/24'
+# )
+# lyriks393_limma <- removeBatchEffect(
+#   lyriks393_corr,
+#   batch = metadata393$extraction_date,
+#   design = mod_group 
+# )
