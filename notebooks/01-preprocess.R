f0ab39d619b3bbe34d68bac058014fa8c3a4ebf1
uuaxe <cweixin@gmail.com>
2026-04-20 18:49:35 +0800

    remove samples with no collection date before correction

--- notebooks/01-preprocess.R
+++ notebooks/01-preprocess.R
@@ -40,43 +40,44 @@ metadata$collection_datetime <- as.POSIXct(
 ### Psychosis signature (cvt, mnt, ctrl) ###
 
 outliers_sn <- c('L0076C', 'L0018C', 'L0626C')
-lyriks393 <- subset_cols(
+missing_sn <- c('L0365S', 'L0417S')
+lyriks387 <- subset_cols(
   lyriks_full,
   metadata,
-  !(sn %in% outliers_sn)
+  !(sn %in% c(outliers_sn, missing_sn))
 )
-print(dim(lyriks393))
+print(dim(lyriks387))
 
-metadata393 <- metadata[colnames(lyriks393), ]
+metadata387 <- metadata[colnames(lyriks387), ]
 # Relevel with convert batch as reference
-metadata393$extraction_date <- droplevels(metadata393$extraction_date)
-print(levels(metadata393$extraction_date))
+metadata387$extraction_date <- droplevels(metadata387$extraction_date)
+print(levels(metadata387$extraction_date))
 
 # Convert to days since earliest 
-metadata393$run_datetime_days <- as.numeric(difftime(
-  metadata393$run_datetime,
-  min(metadata393$run_datetime, na.rm = TRUE),
+metadata387$run_datetime_days <- as.numeric(difftime(
+  metadata387$run_datetime,
+  min(metadata387$run_datetime, na.rm = TRUE),
   units = "days"
 ))
-metadata393$collection_datetime_days <- as.numeric(difftime(
-  metadata393$collection_datetime,
-  min(metadata393$collection_datetime, na.rm = TRUE),
+metadata387$collection_datetime_days <- as.numeric(difftime(
+  metadata387$collection_datetime,
+  min(metadata387$collection_datetime, na.rm = TRUE),
   units = "days"
 ))
-# metadata393$collection_datetime_centered <- 
-#   metadata393$collection_datetime_days - mean(metadata393$collection_datetime_days)
-# mod <- model.matrix(~group, data = metadata393)
+# metadata387$collection_datetime_centered <- 
+#   metadata387$collection_datetime_days - mean(metadata387$collection_datetime_days)
+# mod <- model.matrix(~group, data = metadata387)
 
 ### Subset control samples for slope estimation ###
 
 ctrl_0409 <- subset_cols(
-  lyriks393,
+  lyriks387,
   metadata,
   group == 'Healthy control'
     & extraction_date == '4/9/24'
-    & !(sn %in% outliers_sn)
+    # & !(sn %in% outliers_sn)
 )
-metadata_ctrl_0409 <- metadata393[colnames(ctrl_0409), ] 
+metadata_ctrl_0409 <- metadata387[colnames(ctrl_0409), ] 
 
 mod <- model.matrix(
    ~ run_datetime_days + collection_datetime_days,
@@ -95,43 +96,59 @@ betas <- coef(fit)
 
 ### Batch correction for continuous covariates on entire CMC subset ###
 
-lyriks393_corr <- lyriks393 - outer(
+lyriks387_corr <- lyriks387 - outer(
   betas[, "run_datetime_days"],
-  metadata393$run_datetime_days
+  metadata387$run_datetime_days
 ) - outer(
   betas[, "collection_datetime_days"],
-  metadata393$collection_datetime_days
+  metadata387$collection_datetime_days
 )
 
 
 ### Batch correct for extraction date ###
 
 # ComBat correction
-print(table(metadata393$extraction_date, metadata393$group))
-mod_group <- model.matrix(~ group, data = metadata393)
+print(table(metadata387$extraction_date, metadata387$group))
+mod_group <- model.matrix(~ group, data = metadata387)
 
-lyriks393_combat <- ComBat(
-  lyriks393_corr,
-  batch = metadata393$extraction_date,
+lyriks387_combat <- ComBat(
+  lyriks387_corr,
+  batch = metadata387$extraction_date,
   mod = mod_group,
   par.prior = TRUE,
   ref.batch = '4/9/24'
 )
 
-file <- 'data/tmp/lyriks393-combat_0409.csv'
-write.csv(lyriks393_combat, file)
+if (any(is.na(lyriks387_combat)))
+  cat('Missing values present in corrected data!')
+
+file <- 'data/tmp/lyriks387-combat_0409.csv'
+write.csv(lyriks387_combat, file)
 print(file)
 
 
 # ## Alternative: Limma correction (linear model, no Bayesian shrinkage)
 # # Assumption: No interaction between batch effects due to run_datetime
 # # and extraction_date
-# metadata393$extraction_date <- relevel(
-#   metadata393$extraction_date,
+# metadata387$extraction_date <- relevel(
+#   metadata387$extraction_date,
 #   ref = '4/9/24'
 # )
-# lyriks393_limma <- removeBatchEffect(
-#   lyriks393_corr,
-#   batch = metadata393$extraction_date,
+# lyriks387_limma <- removeBatchEffect(
+#   lyriks387_corr,
+#   batch = metadata387$extraction_date,
 #   design = mod_group 
 # )
+
+
+# # Ad hoc - Check NAs in data
+# # Conclusion: Due to missing collection dates
+# file <- 'data/tmp/lyriks387-combat_0409.csv'
+# lyriks387_combat <- read.csv(file, row.names = 1)
+# 
+# sids <- c('L0365S_12', 'L0417S_24')
+# 
+# lyriks387_combat[, sids]
+# lyriks_full[, sids]
+# lyriks387[, sids]
+# lyriks387_corr[, sids]
